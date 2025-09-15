9d88cb3abb07331b7c52847d1815ac9c33476b5a
Miguel <miguelfale94@gmail.com>
2025-09-15 19:49:11 +0100

    deleted unnecessary stuff

--- misc/withheader/print.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "print.h"
-
-void print_values ( int num ) {
-
-  printf ( "Valor Hexadecimal: %X; Octal: %o; Decimal: %d; Misterio: %u.\n",
-	   num, num, num, num );
-
-} /* end print_values */
